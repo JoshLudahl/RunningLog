0b8d8e0df17faa2fce00be1d974a7e8364ab33d7
Josh Ludahl <jludahl@gmail.com>
2016-10-10 16:23:29 -0700

    Deploy

--- src/main/java/run/makemy/services/currentuser/CurrentUserDetailsService.java
+++ src/main/java/run/makemy/services/currentuser/CurrentUserDetailsService.java
@@ -8,6 +8,8 @@ import run.makemy.domains.user.CurrentUser;
 import run.makemy.domains.user.User;
 import run.makemy.services.user.UserService;
 
+import java.util.function.Supplier;
+
 @Service
 public class CurrentUserDetailsService implements UserDetailsService {
 
@@ -21,7 +23,12 @@ public class CurrentUserDetailsService implements UserDetailsService {
     @Override
     public CurrentUser loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userService.getUserByEmail(email)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format("User with email=%s could not be found", email)));
+                .orElseThrow(new Supplier<UsernameNotFoundException>() {
+                    @Override
+                    public UsernameNotFoundException get() {
+                        return new UsernameNotFoundException(String.format("User with email=%s could not be found", email));
+                    }
+                });
 
         return new CurrentUser(user);
     }
